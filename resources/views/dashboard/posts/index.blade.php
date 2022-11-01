f2e9b01bbdd5ccf5ab5470b3515aa7828785b6d3
Frizka Ade P <106709088+frizkaadep@users.noreply.github.com>
2022-11-01 13:06:04 +0700

    18. Create New Post Form
    a. title
    b. slug => https://github.com/cviebrock/eloquent-sluggable
    c. category => form select with DB category
    d. Body Post => with Trix Editor ( https://github.com/basecamp/trix )

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -5,10 +5,11 @@
         <h1 class="h2">My Posts</h1>
     </div>
     <div class="table-responsive">
+        <a href="/dashboard/posts/create" class="btn btn-primary">Create New Post</a>
         <table class="table table-striped table-sm">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
+                    <th scope="col">No</th>
                     <th scope="col">Title</th>
                     <th scope="col">Category</th>
                     <th scope="col">Action</th>
